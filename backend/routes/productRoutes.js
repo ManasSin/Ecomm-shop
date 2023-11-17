15e1ef61597ce2d0b1ba1507df4185ad66baaaa1
ManasSin <maltisadan12@gmail.com>
2023-11-17 11:19:05 +0530

    simplified router logic

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,12 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../modals/productModal.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// Get all products
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// Get a single product by ID
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    // console.log(product);
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404).json({ message: "Product not found" });
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
