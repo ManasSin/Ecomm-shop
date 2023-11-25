27fab7e7c1e779bb5e61780d096cc736ee67d30a
ManasSin <maltisadan12@gmail.com>
2023-11-25 15:16:14 +0530

    packages added for encryption and authentication

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDb from "./config/db.js";
 import cors from "cors";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
@@ -13,6 +14,13 @@ connectDb(); // connect to database
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// body parser middlware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.use(
   cors({
