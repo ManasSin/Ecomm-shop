e570752cca8a36a32388a8a08f3c4ca134590dbf
ManasSin <maltisadan12@gmail.com>
2023-12-12 22:56:47 +0530

    Update sameSite attribute in generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,9 @@ export const generetToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "development" ? "None" : "Strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
+
+// http = deve.
