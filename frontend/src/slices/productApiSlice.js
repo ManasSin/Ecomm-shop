f40a0ae6da39669e787bf06a42462e9ac84d3c8e
ManasSin <maltisadan12@gmail.com>
2023-12-17 13:43:30 +0530

    Add productApiSlice mutations and update exports

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -1,4 +1,5 @@
 import { PRODUCTS_URL } from "../constants";
+import { UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlices";
 
 export const productApiSlice = apiSlice.injectEndpoints({
@@ -8,6 +9,7 @@ export const productApiSlice = apiSlice.injectEndpoints({
         url: PRODUCTS_URL,
       }),
       keepUnusedDataFor: 10,
+      providesTags: ["Products"],
     }),
     getProdcutsDetails: builder.query({
       query: (productId) => ({
@@ -15,8 +17,35 @@ export const productApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    UpdateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Products"],
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProdcutsDetailsQuery } =
-  productApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProdcutsDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} = productApiSlice;
