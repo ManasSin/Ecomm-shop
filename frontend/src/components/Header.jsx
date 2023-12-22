ccc906b47e407adccfb6af6a712923ea86c5a885
ManasSin <maltisadan12@gmail.com>
2023-12-22 19:02:38 +0530

    Add SearchBox component to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,7 @@ import logo from "../assets/logo.png";
 import { useLogoutMutation } from "../slices/usersAuthSlice";
 import { logout } from "../slices/authSlice";
 import { toast } from "react-toastify";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -41,6 +42,8 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <SearchBox />
+
               <LinkContainer to={"/cart"}>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
@@ -74,7 +77,7 @@ const Header = () => {
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
 
-                  <LinkContainer to={"/admin/userslist"}>
+                  <LinkContainer to={"/admin/userlist"}>
                     <NavDropdown.Item>User</NavDropdown.Item>
                   </LinkContainer>
 
