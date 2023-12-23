b0d034aac015511ba4be5f738b855dce5b76979b
ManasSin <maltisadan12@gmail.com>
2023-12-23 22:19:09 +0530

    Update server.js to serve static files in production

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  console.log(path.resolve(__dirname, "frontend", "dist", "index.html"));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
